1faaad8813a9467fd1a3722bcb527283a5b2b5b4
qaw302 <osshun21@gmail.com>
2021-03-27 16:28:49 +0900

    added func MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -7,7 +7,8 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		
 		while (num !=6) {
-			System.out.println("1. Add Money");
+			System.out.println("*** Money Management System Menu ***");
+			System.out.println("1. Income Money");
 			System.out.println("2. Delete Money");
 			System.out.println("3. Edit Money");
 			System.out.println("4. View Money");
@@ -15,25 +16,54 @@ public class MenuManager {
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6");
 			num = input.nextInt();
-			switch(num) {
-			case 1:
-				System.out.println("Date : ");
-				String date = input.next();
-				System.out.println("Income : ");
-				int income = input.nextInt();
-				System.out.println("Note : ");
-				String note = input.next();
-				break;
-			case 2:
-				System.out.println("Income : ");
-				int income2 = input.nextInt();
-			case 3:
-				
-			case 4:
-				
-				
+			if (num == 1) {
+				addMoney();
 			}
+			else if (num == 2) {
+				deleteMoney();
+			}
+			else if (num == 3) {
+				editMoney();
+			}
+			else if (num == 4) {
+				viewMoney();
+			}
+			else {
+				continue;
+			}
+			
 		}
+		
+	}
+	
+	public static void addMoney() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Income/Expenses : ");
+		String ie = input.next();
+		System.out.print("Date : ");
+		String date = input.next();
+		System.out.print("Amount : ");
+		int amount = input.nextInt();
+		System.out.print("Note : ");
+		String note = input.next();
+		
+	}
+	
+	public static void deleteMoney() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("");
+		
+	}
+	
+	public static void editMoney() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("");
+
+	}
+	
+	public static void viewMoney() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("");
 		
 	}
 
